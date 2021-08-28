fe7abe9f37db71a596b6d3b469f035bb5547976e
jonathan-jota <jonathan.jota.santos@gmail.com>
2021-08-28 00:08:52 -0300

    creatd person class

--- digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Jonathan"
+    var cpf: String = "123.456.789-00"
+}
+
+fun main () {
+    val jonathan = Pessoa()
+
+    println(jonathan.nome)
+    println(jonathan.cpf)
 }
