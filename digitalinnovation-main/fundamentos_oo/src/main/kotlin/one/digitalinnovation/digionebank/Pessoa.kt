57937ad84753ed49f2effbc49d66a507d683d7ae
jonathan-jota <jonathan.jota.santos@gmail.com>
2021-08-28 00:21:09 -0300

    Updated to apply the private modifier to set function of cpf property

--- digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,7 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Jonathan"
     var cpf: String = "123.456.789-00"
+    private set //impede a alteração do cpf
 }
 
 fun main () {
