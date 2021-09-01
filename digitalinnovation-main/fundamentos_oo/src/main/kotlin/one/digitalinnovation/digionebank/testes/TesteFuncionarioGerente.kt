71a8f32160be48c964544d657e5dfdaf66540e31
jonathan-jota <jonathan.jota.santos@gmail.com>
2021-08-31 23:43:59 -0300

    created clients, logavel and tests

--- digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,7 +4,11 @@ import one.digitalinnovation.digionebank.Gerente
 
 
 fun main() {
-    val sofia = Gerente("Sofia Gregorio", "123.456.789-0", 6000.0)
+    val sofia = Gerente("Sofia Gregorio", "123.456.789-0", 6000.0, "senha1234")
+
     ImprimeRelatorioFuncionario.imprime(sofia)
+
+    TesteAutenticacao().autentica(sofia)
+
 }
 
